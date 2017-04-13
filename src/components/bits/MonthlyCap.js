08e04e1102e6abed8933059862edeca895c816b6
carla stockalper <cstockalper@gmail.com>
2017-04-13 14:21:14 -0400

    css

--- src/components/bits/MonthlyCap.js
+++ src/components/bits/MonthlyCap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ScrollView,
   StyleSheet,
@@ -7,7 +7,7 @@ import {
   Button,
   TouchableHighlight,
   Image
-} from 'react-native';
+} from "react-native";
 
 export class MonthlyCap extends React.Component {
   constructor(props) {
@@ -16,14 +16,20 @@ export class MonthlyCap extends React.Component {
 
   render() {
     return (
-      <View>
-        <TouchableHighlight onPress={this.props.goBack}>
-          <Text>
-            go back!
+      <View style={styles.container}>
+        <TouchableHighlight
+          onPress={this.props.goBack}
+          style={styles.backArrow}
+        >
+          <Text style={styles.arrowText}>
+            BACK
           </Text>
         </TouchableHighlight>
-        <Text>
-          Monthly Cap
+        <Text style={styles.headline}>
+          HOW MUCH WILL YOU GIVE?
+        </Text>
+        <Text style={styles.paragraph}>
+          Use the buttons below to set your monthly donation cap. Adjust the cap at any time, your monthly donation will never excceed the amount you set below.
         </Text>
 
         <View style={styles.counter}>
@@ -32,18 +38,27 @@ export class MonthlyCap extends React.Component {
               {this.props.monthlyCap}
             </Text>
           </View>
-          <View>
-            <TouchableHighlight onPress={this.props.handleIncreaseCap}>
-              <Text>Boost It!</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableHighlight
+              onPress={this.props.handleIncreaseCap}
+              style={styles.buttonAdd}
+            >
+              <Text style={styles.buttonText}>+</Text>
             </TouchableHighlight>
-            <TouchableHighlight onPress={this.props.handleDecreaseCap}>
-              <Text>Decrease It!</Text>
+            <TouchableHighlight
+              onPress={this.props.handleDecreaseCap}
+              style={styles.buttonMinus}
+            >
+              <Text style={styles.buttonText}>-</Text>
             </TouchableHighlight>
           </View>
-          <TouchableHighlight onPress={this.props.onDoneMonthlyCapPress} style={styles.save}>
-            <Text>Save New Cap</Text>
-          </TouchableHighlight>
         </View>
+        <TouchableHighlight
+          onPress={this.props.onDoneMonthlyCapPress}
+          style={styles.save}
+        >
+          <Text style={styles.capText}>SAVE</Text>
+        </TouchableHighlight>
 
       </View>
     );
@@ -54,16 +69,90 @@ const styles = StyleSheet.create({
   counter: {
     padding: 20,
     // flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'rgba(100,149,237, 1)',
-    width: '100%'
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#eee",
+    width: "100%",
+    marginTop: 30
   },
   save: {
     padding: 20,
     // flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'rgba(100,34,86, 1)'
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#68DEA8",
+    borderRadius: 20,
+    height: 50,
+    width: 200,
+    marginTop: 40
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fafafa"
+  },
+  backArrow: {
+    position: "absolute",
+    top: 25,
+    left: 25,
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#68DEA8",
+    height: 30,
+    width: 50,
+    borderRadius: 20
+  },
+  arrowText: {
+    color: "#fafafa",
+    fontSize: 12,
+    textAlign: "center",
+    alignSelf: "center"
+  },
+  capText: {
+    textAlign: "center",
+    width: "80%",
+    marginBottom: 40
+  },
+  headline: {
+    textAlign: "center",
+    fontWeight: "700",
+    marginBottom: 10
+  },
+  paragraph: {
+    textAlign: "center",
+    width: "90%"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30
+  },
+  buttonAdd: {
+    height: 40,
+    width: 80,
+    borderBottomLeftRadius: 20,
+    borderTopLeftRadius: 20,
+    borderWidth: 0.8,
+    borderColor: "#fafafa",
+    backgroundColor: "#68DEA8",
+    overflow: "hidden"
+  },
+  buttonMinus: {
+    height: 40,
+    width: 80,
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20,
+    borderWidth: 0.8,
+    borderColor: "#fafafa",
+    backgroundColor: "#68DEA8",
+    overflow: "hidden"
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: "center",
+    justifyContent: "center"
   }
 });
